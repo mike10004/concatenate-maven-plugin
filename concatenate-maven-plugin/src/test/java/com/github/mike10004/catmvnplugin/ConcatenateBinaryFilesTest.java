436c501f4536286edc045fdeebde663798af3b7e
Mike Chaberski <mchaberski@gmail.com>
2018-12-11 14:01:25 -0500

    ConcatenateBinaryFilesTest: sort files to make ordering deterministic

--- concatenate-maven-plugin/src/test/java/com/github/mike10004/catmvnplugin/ConcatenateBinaryFilesTest.java
+++ concatenate-maven-plugin/src/test/java/com/github/mike10004/catmvnplugin/ConcatenateBinaryFilesTest.java
@@ -1,5 +1,6 @@
 package com.github.mike10004.catmvnplugin;
 
+import com.google.common.io.BaseEncoding;
 import com.google.common.io.Files;
 import org.apache.commons.lang3.ArrayUtils;
 import org.junit.Before;
@@ -8,6 +9,7 @@ import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Random;
 
@@ -28,12 +30,12 @@ public class ConcatenateBinaryFilesTest {
     @Test
     public void execute_basic() throws Exception {
         File dir = temporaryFolder.newFolder();
-        File someFile1 = new File(dir, "child/foo.bin"), someFile2 = new File(dir, "child/bar.bin");
+        File someFile1 = new File(dir, "child/part1.bin"), someFile2 = new File(dir, "child/part2.bin");
         Files.createParentDirs(someFile1);
         Files.createParentDirs(someFile2);
-        byte[] head = new byte[256];
+        byte[] head = new byte[64];
         random.nextBytes(head);
-        byte[] tail = new byte[256];
+        byte[] tail = new byte[64];
         random.nextBytes(tail);
         byte[] expected = ArrayUtils.addAll(head, tail);
         Files.asByteSink(someFile1).write(head);
@@ -41,13 +43,18 @@ public class ConcatenateBinaryFilesTest {
         OrderableFileSet fileset = new OrderableFileSet();
         fileset.setDirectory(dir.getAbsolutePath());
         fileset.setIncludes(Collections.singletonList("**/*.bin"));
+        fileset.setSort(OrderableFileSet.SortingStrategy.alphabetical);
         File outputFile = new File(temporaryFolder.newFolder(), "output.bin");
         ConcatenateMojo mojo = new ConcatenateMojo();
         mojo.setOutputFile(outputFile);
         mojo.setSources(new OrderableFileSet[]{fileset});
         mojo.execute();
-        byte[] outputBytes = Files.toByteArray(outputFile);
-        assertArrayEquals("output file bytes should equal concatenation of input files bytes", expected, outputBytes);
+        byte[] actual = Files.toByteArray(outputFile);
+        if (!Arrays.equals(expected, actual)) {
+            System.out.format("expected %s%n", BaseEncoding.base16().encode(expected));
+            System.out.format("  actual %s%n", BaseEncoding.base16().encode(actual));
+        }
+        assertArrayEquals("output file bytes should equal concatenation of input files bytes", expected, actual);
     }
 
 }
