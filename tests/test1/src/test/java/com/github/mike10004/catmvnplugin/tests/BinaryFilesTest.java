9f81cea9aa11d40c75825f2134e3eb67f8108207
Mike Chaberski <mchaberski@gmail.com>
2018-12-11 12:02:40 -0500

    add integration test demonstrating issue #1 (failure to concatenate binary files)

---
+++ tests/test1/src/test/java/com/github/mike10004/catmvnplugin/tests/BinaryFilesTest.java
@@ -0,0 +1,21 @@
+package com.github.mike10004.catmvnplugin.tests;
+
+import com.google.common.io.Files;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class BinaryFilesTest extends CatTestBase {
+
+    @Rule
+    public TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+    @Test
+    public void concatenate_orderingStrategyStrict() throws Exception {
+        byte[] expected = Files.asByteSource(getTestInputFile("cat.jpg")).read();
+        byte[] actual = Files.asByteSource(getOutputFile()).read();
+        assertArrayEquals("output", expected, actual);
+    }
+}
--- tests/test1/src/test/java/com/github/mike10004/catmvnplugin/tests/CatTestBase.java
+++ tests/test1/src/test/java/com/github/mike10004/catmvnplugin/tests/CatTestBase.java
@@ -1,5 +1,7 @@
 package com.github.mike10004.catmvnplugin.tests;
 
+import com.google.common.base.Preconditions;
+
 import java.io.File;
 
 public class CatTestBase {
@@ -10,5 +12,18 @@ public class CatTestBase {
 
     protected File getOutputFile() {
         return new File(System.getProperty(getOutputFileSystemPropertyName()));
+    }
+
+    protected File getTestInputFile(String relativePath) {
+        Preconditions.checkArgument(relativePath != null && !relativePath.isEmpty(), "relative path must be non-null and non-empty");
+        return new File(getTestInputDirectory(), relativePath);
+    }
+
+    protected File getTestInputDirectory() {
+        String propValue = System.getProperty("testInputFilesDirectory");
+        Preconditions.checkState(propValue != null, "test input files directory is not defined as system property testInputFilesDirectory");
+        File dir = new File(propValue);
+        Preconditions.checkState(dir.isDirectory(), "not a directory: " + dir);
+        return dir;
     }
 }
